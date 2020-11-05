21ee200be5c5627b198d97c92af2570f43477161
naco12 <nacop09@gmail.com>
2020-11-05 11:18:19 -0600

    completed module

--- stats/data.py
+++ stats/data.py
@@ -11,10 +11,10 @@ game_files.sort()
 
 game_frames = []
 for game_file in game_files:
-    game_frame = pd.read_csv('game_file', names =  ['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
-    game_frames.append('game_frame')
+    game_frame = pd.read_csv(game_file, names =  ['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
+    game_frames.append(game_frame)
     #concatenate Dataframes
-    games = pd.concat('game_frames')
+    games = pd.concat(game_frames)
 
 # dataframe Cleaning
-games.loc[games['multi5']==??], ['multi5']] = ''
+games.loc[[games['multi5']=='??'], ['multi5']] = ''
