96f02f58ba6cf42cebd717d33fdbbb00ca0a7e32
naco12 <nacop09@gmail.com>
2020-11-05 16:13:18 -0600

    completed module

---
+++ stats/attendance.py
@@ -0,0 +1,19 @@
+# importing libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+from data import games
+
+attendance = games.loc[games['type'] == 'info' and games['multi2'] == 'attendance', ['year', 'multi3']]
+
+attendance.columns = ['year', 'attendance']
+
+attendance.loc[:,['attendance']] = pd.to_numeric(attendance.loc[:,['attendance']])
+
+attendance.plot(x='year', y='attendance', figsize=(15, 7), kind='bar')
+
+plt.xlabel('Year')
+plt.ylabel('Attendance')
+
+plt.axhline(dataframe['year'].mean(), label='year', linestyle='dashed', color='green')
+
+plt.show()
