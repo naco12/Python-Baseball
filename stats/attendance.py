9335a7f75c9abb43a368402d23d9863faaa0c178
naco12 <nacop09@gmail.com>
2020-11-05 16:32:47 -0600

    completed module

--- stats/attendance.py
+++ stats/attendance.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from data import games
 
-attendance = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendance'), ['year', 'multi3']]
+attendance = games.loc[games['type'] == 'info', ['year', 'multi3']]
+attendance = games.loc[games['multi2'] == 'attendance', ['year', 'multi3']]
 
 attendance.columns = ['year', 'attendance']
 
