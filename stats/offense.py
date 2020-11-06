c2638418e399d8db61ff303290a305a869ae9b28
naco12 <nacop09@gmail.com>
2020-11-05 22:56:07 -0600

    completed module

--- stats/offense.py
+++ stats/offense.py
@@ -4,3 +4,4 @@ from data import games
 
 plays = games[games['type']=='play']
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
+hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'), ['inning', 'event']]
