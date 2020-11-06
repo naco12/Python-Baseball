5fb038c47af863ff707a681ddff32b7bf2f6a15e
naco12 <nacop09@gmail.com>
2020-11-06 15:38:27 -0600

    completed module

--- stats/defense.py
+++ stats/defense.py
@@ -11,3 +11,6 @@ events = events.unstack().fillna(0).reset_index()
 events.columns = events.columns.droplevel()
 events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
 events = events.rename_axis(None, axis='columns')
+
+#Merge Plate Appearances
+events_plus_pa = pd.merge(events, pa, how='outer', left_on = ['year', 'game_id', 'team'], right_on = ['year', 'game_id', 'team'])
