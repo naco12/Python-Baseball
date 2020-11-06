9f435a47f0e42ab932edcda9540361ace27170b3
naco12 <nacop09@gmail.com>
2020-11-05 22:04:50 -0600

    completed module

--- stats/pitching.py
+++ stats/pitching.py
@@ -11,3 +11,5 @@ strike_outs = strike_outs.groupby(['year', 'game_id']).size()
 
 #reset index
 strike_outs = strike_outs.reset_index(name='strike_outs')
+
+strike_outs = strike_outs.loc[:,['year', 'strike_outs']].apply(pd.to_numeric())
