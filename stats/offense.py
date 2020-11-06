46cd2cd8acc2310f13df8a05d924f891c43328df
naco12 <nacop09@gmail.com>
2020-11-06 09:33:16 -0600

    completed module

--- stats/offense.py
+++ stats/offense.py
@@ -18,3 +18,4 @@ r'^HR(.*)': 'hr'
 hit_type = hits['event'].replace(replacements, regex=True)
 hits =hits.assign(hit_type=hit_type)
 hits = hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
+pd.categorical(hits['hit_type'], ['single', 'double', 'triple', 'hr'])
