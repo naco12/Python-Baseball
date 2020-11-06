208ff35d4e99fab590ec782bad04719a10011856
naco12 <nacop09@gmail.com>
2020-11-05 21:55:47 -0600

    completed module

--- stats/pitching.py
+++ stats/pitching.py
@@ -8,3 +8,6 @@ strike_outs = plays[plays['event'].str.contains('K')]
 
 #group by year and game
 strike_outs = strike_outs.groupby(['year', 'game_id']).size()
+
+#reset index
+strike_outs = strike_outs.reset_index(name='strike_outs')
