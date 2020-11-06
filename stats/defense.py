19aa09456240521d0b0768ee42bb460060756f3c
naco12 <nacop09@gmail.com>
2020-11-06 15:00:20 -0600

    completed module

--- stats/defense.py
+++ stats/defense.py
@@ -6,3 +6,4 @@ plays = games.query("type=='play' & event != 'NP'")
 plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
 pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', 'inning', 'team', 'player']]
 pa = pa.groupby(['year', 'game_id', 'team']).size().reset_index(name ='PA')
+events = events.set_index(['year', 'game_id', 'team', 'event_type'])
