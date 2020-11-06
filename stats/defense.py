ccb97957ce6d693a118877edd7541d0053b9ae15
naco12 <nacop09@gmail.com>
2020-11-06 12:29:45 -0600

    completed module

--- stats/defense.py
+++ stats/defense.py
@@ -2,4 +2,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from frames import games, info, events
 
-plays = games.query('games['type']=='play' & games['event']!='NP'')
+plays = games.query('games['type']=='play'')
+plays = games.query('games['event']!='NP'')
