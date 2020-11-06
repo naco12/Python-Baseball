b28ec2b0a5c05da897fd35ebaecf0a9f40b96bb9
naco12 <nacop09@gmail.com>
2020-11-06 13:26:40 -0600

    completed module

--- stats/defense.py
+++ stats/defense.py
@@ -3,3 +3,4 @@ import matplotlib.pyplot as plt
 from frames import games, info, events
 
 plays = games.query("type=='play' & event != 'NP'")
+plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event', 'game_id', 'year']
