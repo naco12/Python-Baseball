f6d8a1a7625472da69ce33c03feb5def7b2fbbf4
naco12 <nacop09@gmail.com>
2020-11-06 15:13:57 -0600

    completed module

--- stats/defense.py
+++ stats/defense.py
@@ -8,3 +8,6 @@ pa = plays.loc[plays['player'].shift() != plays['player'], ['year', 'game_id', '
 pa = pa.groupby(['year', 'game_id', 'team']).size().reset_index(name ='PA')
 events = events.set_index(['year', 'game_id', 'team', 'event_type'])
 events = events.unstack().fillna(0).reset_index()
+events.columns = events.columns.droplevel()
+events.columns = ['year', 'game_id', 'team', 'BB', 'E', 'H', 'HBP', 'HR', 'ROE', 'SO']
+events = rename_axis(label=None, axis=1)
