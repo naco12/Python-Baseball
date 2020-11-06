d9d8be67db7300a6ccfb6ced1c4fc4726bae6540
naco12 <nacop09@gmail.com>
2020-11-05 21:36:27 -0600

    completed module

--- stats/pitching.py
+++ stats/pitching.py
@@ -5,3 +5,6 @@ from data import games
 plays = games[games['type']=='play']
 
 strike_outs = plays[plays['event'].str.contains('K')]
+
+#group by year and game
+strike_outs = strike_outs.groupby('year', 'game_id').size()
