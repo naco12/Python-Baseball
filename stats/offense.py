e981c64163c1584c3f24027b59556b7212e6051e
naco12 <nacop09@gmail.com>
2020-11-06 09:56:39 -0600

    completed module

--- stats/offense.py
+++ stats/offense.py
@@ -22,3 +22,5 @@ hits['hit_type'] = pd.Categorical(hits['hit_type'], ['single', 'double', 'triple
 hits = hits.sort_values(['inning', 'hit_type'])
 hits = hits.pivot(index='inning', columns='hit_type', values='count')
 hits.plot.bar(stacked=True)
+
+plt.show()
